f4d2b1d1b469be196359850ecbb0dec303fa297a
drbea <beavoguimaximeakoi@gmail.com>
2024-05-29 19:47:34 +0200

    definition de premiere structure des elements de la BDD de l'application blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=150)
+    description = models.TextField(null = True, blank = True)
+    category = models.ForeignKey("blog.Category", on_delete=models.CASCADE)
+
+
+class Command(models.Model):
+
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey("blog.Product", on_delete=models.CASCADE)
+    quantity = models.IntegerField(default = 0)
